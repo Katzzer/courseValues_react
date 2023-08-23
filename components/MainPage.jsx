cf7b473ced5f96afa66f6f5afbf4966d6670f940
Pavel Kostal <kostal@quantretail.com>
2023-08-24 00:19:50 +0200

    added buttons that calls different API

--- components/MainPage.jsx
+++ components/MainPage.jsx
@@ -3,21 +3,32 @@ import axios from "axios";
 import CourseValue from "./CourseValue.jsx";
 
 const backendUrl = "https://webapi.developers.erstegroup.com/api/csas/public/sandbox/v2/rates/exchangerates?web-api-key=c52a0682-4806-4903-828f-6cc66508329e"
+const springBootApi = "http://localhost:8080/api/v1/course-tickets"
 
 function MainPage() {
 
     const [courseValues, setCourseValues] = useState(null)
-
-    useEffect(() => {
+    
+    function getDataDirectlyFromPublicApi() {
         axios.get(backendUrl).then(response => {
             setCourseValues(response.data)
         })
-        
-    }, []);
+    }
+    
+    function getDataFromSpringBootApi(usedb) {
+        const url = usedb ? springBootApi + "?usedb=true" : springBootApi + "?usedb=false"
+        console.log(url)
+        axios.get(url).then(response => {
+            setCourseValues(response.data)
+        })
+    }
     
     return (
         <>
             <h1>Course values</h1>
+            <button onClick={getDataDirectlyFromPublicApi}>Get data directly from public API</button>
+            <button onClick={() => getDataFromSpringBootApi(false)}>Get data from spring boot app (no db)</button>
+            <button onClick={() => getDataFromSpringBootApi(true)}>Get data from spring boot app (using db)</button>
 
             {courseValues && courseValues.map(courseValue => (
                 <CourseValue
